c7da99be46b385734dc994113e65cb4ddd1e61b4
krisly8386 <krisly8386@gmail.com>
2025-06-23 22:05:52 -0500

    Update the extra credit

--- homework/lower_precision.py
+++ homework/lower_precision.py
@@ -5,7 +5,10 @@ from .qlora import QLoRABigNet
 def load(path: Path | None):
     # Extra credit: A BigNet variant that uses <4 bits/parameter and retains decent accuracy
     # A <9MB compressed model using 4-bit quantization + LoRA
-    model = QLoRABigNet(lora_dim=4, group_size=64)  # smaller LoRA + larger group size = fewer float32 values
+    model = QLoRABigNet(
+        lora_dim=2,        # reduce to smallest possible
+        group_size=128     # increase group size
+    )
 
     if path is not None:
         model.load_state_dict(torch.load(path, weights_only=True), strict=False)
