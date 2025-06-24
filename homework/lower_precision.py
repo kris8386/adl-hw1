733b2961f1857828d3344874c27fa50bdd700078
krisly8386 <krisly8386@gmail.com>
2025-06-23 21:56:10 -0500

    Update the extra credit

--- homework/lower_precision.py
+++ homework/lower_precision.py
@@ -1,8 +1,12 @@
 from pathlib import Path
-
+import torch
+from .qlora import QLoRABigNet  
 
 def load(path: Path | None):
-    # TODO (extra credit): Implement a BigNet that uses in
-    # average less than 4 bits per parameter (<9MB)
-    # Make sure the network retains some decent accuracy
-    return None
+    # Extra credit: A BigNet variant that uses <4 bits/parameter and retains decent accuracy
+    model = QLoRABigNet(lora_dim=32, group_size=16)
+    if path is not None:
+        state_dict = torch.load(path, weights_only=True)
+        model.load_state_dict(state_dict, strict=False)
+    return model
+
