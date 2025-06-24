3b467cb48265d4d8b2e5116101e96b4bf43ce83a
krisly8386 <krisly8386@gmail.com>
2025-06-23 23:15:43 -0500

    Update the extra credit

--- homework/lower_precision.py
+++ homework/lower_precision.py
@@ -5,7 +5,7 @@ from .qlora import QLoRABigNet  # Adjust if QLoRABigNet is in a different locati
 
 def load(path: Path | None):
     # Extra credit: A BigNet variant that uses <4 bits/parameter and retains decent accuracy
-    model = QLoRABigNet(lora_dim=1, group_size=8192*2)
+    model = QLoRABigNet(lora_dim=2, group_size=8192)
     if path is not None:
         state_dict = torch.load(path, weights_only=True)
         model.load_state_dict(state_dict, strict=False)
