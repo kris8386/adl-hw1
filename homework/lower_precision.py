3ae2b382e56cda6c08bb50c832df4e9cd55a5169
krisly8386 <krisly8386@gmail.com>
2025-06-23 22:07:09 -0500

    Update the extra credit

--- homework/lower_precision.py
+++ homework/lower_precision.py
@@ -6,8 +6,8 @@ def load(path: Path | None):
     # Extra credit: A BigNet variant that uses <4 bits/parameter and retains decent accuracy
     # A <9MB compressed model using 4-bit quantization + LoRA
     model = QLoRABigNet(
-        lora_dim=2,        # reduce to smallest possible
-        group_size=128     # increase group size
+        lora_dim=1,        # smallest LoRA size
+        group_size=256     # fewer float16 normalization terms
     )
 
     if path is not None:
