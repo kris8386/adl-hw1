44154db78cc3af1f0d7dbed603b26a22d0956fdd
krisly8386 <krisly8386@gmail.com>
2025-06-23 22:43:22 -0500

    Update the extra credit

--- homework/lower_precision.py
+++ homework/lower_precision.py
@@ -6,7 +6,7 @@ from .qlora import QLoRABigNet  # Adjust if QLoRABigNet is in a different locati
 def load(path: Path | None):
     # Extra credit: A BigNet variant that uses <4 bits/parameter and retains decent accuracy
     # Adjusted to meet the 9MB memory limit
-    model = QLoRABigNet(lora_dim=1, group_size=512, bias=False)  # <- memory optimizations
+    model = QLoRABigNet(lora_dim=1, group_size=1024, bias=False)  # <- memory optimizations
 
     if path is not None:
         state_dict = torch.load(path, weights_only=True)
